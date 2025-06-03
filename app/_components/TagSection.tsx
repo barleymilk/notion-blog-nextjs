5f17714896500ed310d95377337c9f885668d668
barleymilk <barleymilk640@gmail.com>
2025-06-03 15:35:00 +0900

    Update: Notion API 연동 및 블로그 포스트 데이터 처리 로직 추가

--- app/_components/TagSection.tsx
+++ app/_components/TagSection.tsx
@@ -1,12 +1,30 @@
+'use client';
+
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import Link from 'next/link';
-import { NotionTag } from '@/types/blog';
+import { useSearchParams, usePathname } from 'next/navigation';
+import { TagFilterItem } from '@/types/blog';
+import { cn } from '@/lib/utils';
 
 interface TagSectionProps {
-  tags: NotionTag[];
+  tags: TagFilterItem[];
+  selectedTag: string;
 }
 
-export function TagSection({ tags }: TagSectionProps) {
+export function TagSection({ tags, selectedTag }: TagSectionProps) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const createPageURL = (tagName: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (tagName === '전체') {
+      params.delete('tag');
+    } else {
+      params.set('tag', tagName);
+    }
+    return `${pathname}?${params.toString()}`;
+  };
+
   return (
     <Card>
       <CardHeader>
@@ -15,8 +33,13 @@ export function TagSection({ tags }: TagSectionProps) {
       <CardContent>
         <div className="flex flex-col gap-3">
           {tags.map((tag) => (
-            <Link href={`?tag=${tag.name}`} key={tag.name}>
-              <div className="hover:bg-muted-foreground/10 text-muted-foreground flex items-center justify-between rounded-md p-1.5 text-sm transition-colors">
+            <Link href={createPageURL(tag.name)} key={tag.name} scroll={false} shallow={true}>
+              <div
+                className={cn(
+                  'hover:bg-muted-foreground/10 text-muted-foreground flex items-center justify-between rounded-md p-1.5 text-sm transition-colors',
+                  selectedTag === tag.name && 'bg-muted-foreground/10 text-foreground font-medium'
+                )}
+              >
                 <span>{tag.name}</span>
                 <span>{tag.count}</span>
               </div>
