eb2ddb6e61fb9a7bb0d1df232ddc1a402a5925b5
barleymilk <barleymilk640@gmail.com>
2025-06-04 14:39:02 +0900

    Refactor: 블로그 페이지 구조 개선 및 새로운 HeaderSection 및 PostList 컴포넌트 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import { PostCard } from '@/components/features/blog/PostCard';
 import { TagSection } from '@/app/_components/TagSection';
 import { ProfileSection } from '@/app/_components/ProfileSection';
 import { ContactSection } from '@/app/_components/ContactSection';
-import Link from 'next/link';
 import { getPublishedPosts, getTags } from '@/lib/notion';
-import SortSelect from './_components/client/SortSelect';
+import HeaderSection from './_components/HeaderSection';
+import PostList from '@/components/features/blog/PostList';
 
 interface HomeProps {
   searchParams: Promise<{ tag?: string; sort?: string }>;
@@ -18,8 +17,6 @@ export default async function Home({ searchParams }: HomeProps) {
     getPublishedPosts(selectedTag, selectedSort),
     getTags(),
   ]);
-  // const posts = await getPublishedPosts(selectedTag, selectedSort);
-  // const tags = await getTags(posts);
 
   return (
     <div className="container py-8">
@@ -30,19 +27,10 @@ export default async function Home({ searchParams }: HomeProps) {
         </aside>
         <div className="space-y-8">
           {/* 섹션 제목 */}
-          <div className="flex items-center justify-between">
-            <h2 className="text-3xl font-bold tracking-tight">블로그 목록</h2>
-            <SortSelect />
-          </div>
+          <HeaderSection selectedTag={selectedTag} />
 
           {/* 블로그 카드 그리드 */}
-          <div className="grid gap-4">
-            {posts.map((post) => (
-              <Link href={`/blog/${post.slug}`} key={post.id}>
-                <PostCard post={post} />
-              </Link>
-            ))}
-          </div>
+          <PostList posts={posts} />
         </div>
         {/* 우측 사이드바 */}
         <aside className="flex flex-col gap-6">
