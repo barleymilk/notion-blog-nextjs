fb9d2a074dde7c65380465a09c72b6381a3b46ec
barleymilk <barleymilk640@gmail.com>
2025-06-13 14:00:22 +0900

    Enhance: OG 이미지 및 로봇 메타데이터 생성 기능 추가, 사이트 URL 환경 변수로 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export const metadata: Metadata = {
     telephone: false,
     address: false,
   },
-  metadataBase: new URL('https://notion-blog-nextjs-two-gray.vercel.app/'),
+  metadataBase: new URL(`${process.env.NEXT_PUBLIC_SITE_URL}`),
   alternates: {
     canonical: '/',
   },
